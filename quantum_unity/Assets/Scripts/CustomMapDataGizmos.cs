6274aadc82917a10dd55dd761056c962fbfe7733
ipodtouch0218 <ipodtouch0218@gmail.com>
2024-06-17 23:24:36 -0400

    jh

--- quantum_unity/Assets/Scripts/CustomMapDataGizmos.cs
+++ quantum_unity/Assets/Scripts/CustomMapDataGizmos.cs
@@ -13,12 +13,34 @@ public class CustomMapDataGizmos : MonoBehaviour {
 
     public void OnDrawGizmos() {
         MapCustomData customData = UnityDB.FindAsset<MapCustomDataAsset>(data.Asset.Settings.UserAsset.Id).Settings;
+        GameObject obj = GameObject.FindGameObjectWithTag("Maze");
 
         Gizmos.color = Color.yellow;
-        foreach (MapCustomData.MazeData maze in customData.Mazes) {
+        for (int i = 0; i < customData.Mazes.Length; i++) {
+            if (!obj.transform.GetChild(i).gameObject.activeInHierarchy) {
+                continue;
+            }
+            MapCustomData.MazeData maze = customData.Mazes[i];
             Gizmos.DrawWireCube(
                 (maze.Origin + (maze.Size / 2)).XOY.ToUnityVector3() - new Vector3(0.5f, 0, 0.5f),
                 maze.Size.XOY.ToUnityVector3());
+
+        }
+
+        foreach (var spawnpoint in GameObject.FindGameObjectsWithTag("Spawnpoint")) {
+            float rotation = Mathf.Repeat(spawnpoint.transform.eulerAngles.y, 360);
+            int index = rotation switch {
+                < 45 => 1,
+                < 135 => 2,
+                < 225 => 3,
+                < 315 => 0,
+                _ => 1
+            };
+            Gizmos.DrawIcon(spawnpoint.transform.position, "spawnpoint" + index, true);
+        }
+
+        foreach (var fruit in GameObject.FindGameObjectsWithTag("Fruit Spawnpoint")) {
+            Gizmos.DrawIcon(fruit.transform.position, "fruit", true);
         }
     }
 }
